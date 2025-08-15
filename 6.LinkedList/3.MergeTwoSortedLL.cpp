86e732102db23ea79d733b768f4d20a1cfa81fb7
sgwrld-999 <130132265+sgwrld-999@users.noreply.github.com>
2025-08-15 23:20:02 +0530

    Add ListNode definition for Merge Two Sorted Lists implementation

--- 6.LinkedList/3.MergeTwoSortedLL.cpp
+++ 6.LinkedList/3.MergeTwoSortedLL.cpp
@@ -8,6 +8,15 @@ Explanation : https://leetcode.com/problems/merge-two-sorted-lists/solutions/268
 #include<bits/stdc++.h>
 using namespace std;
 
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
